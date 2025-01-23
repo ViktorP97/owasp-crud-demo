000ecfb6e643c9a770ac03a59630df237377c191
viktorprusak <viktor.prusak@applifting.cz>
2025-01-23 18:58:29 +0100

    fix(app): search

--- owasp-crud-demo/src/main/kotlin/org/example/owaspcruddemo/repository/ItemRepository.kt
+++ owasp-crud-demo/src/main/kotlin/org/example/owaspcruddemo/repository/ItemRepository.kt
@@ -2,15 +2,6 @@ package org.example.owaspcruddemo.repository
 
 import org.example.owaspcruddemo.model.Item
 import org.springframework.data.mongodb.repository.MongoRepository
-import java.time.Instant
 import java.util.UUID
 
-interface ItemRepository : MongoRepository<Item, UUID> {
-    fun findByNameContainsIgnoreCase(name: String): List<Item>
-    fun findByNameContainsIgnoreCaseAndTimeBetween(name: String, time: Instant, time2: Instant): List<Item>
-    fun findByNameContainsIgnoreCaseAndTimeAfter(name: String, time: Instant): List<Item>
-    fun findByNameContainsIgnoreCaseAndTimeBefore(name: String, time: Instant): List<Item>
-    fun findByTimeBetween(time: Instant, time2: Instant): List<Item>
-    fun findByTimeAfter(time: Instant): List<Item>
-    fun findByTimeBefore(time: Instant): List<Item>
-}
+interface ItemRepository : MongoRepository<Item, UUID>
--- owasp-crud-demo/src/main/kotlin/org/example/owaspcruddemo/usecase/SearchItems.kt
+++ owasp-crud-demo/src/main/kotlin/org/example/owaspcruddemo/usecase/SearchItems.kt
@@ -2,37 +2,31 @@ package org.example.owaspcruddemo.usecase
 
 import org.example.owaspcruddemo.model.Item
 import org.example.owaspcruddemo.model.SearchCriteria
-import org.example.owaspcruddemo.repository.ItemRepository
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
 import org.springframework.stereotype.Component
+
 
 @Component
 class SearchItems(
-    private val itemRepository: ItemRepository
+    private val mongoTemplate: MongoTemplate
 ) {
-     fun execute(criteria: SearchCriteria): List<Item> {
-        return when {
-            criteria.name != null && criteria.after != null && criteria.before != null ->
-                itemRepository.findByNameContainsIgnoreCaseAndTimeBetween(criteria.name, criteria.after, criteria.before)
+    fun execute(criteria: SearchCriteria): List<Item> {
+        val query = Query()
 
-            criteria.name != null && criteria.after != null ->
-                itemRepository.findByNameContainsIgnoreCaseAndTimeAfter(criteria.name, criteria.after)
-
-            criteria.name != null && criteria.before != null ->
-                itemRepository.findByNameContainsIgnoreCaseAndTimeBefore(criteria.name, criteria.before)
-
-            criteria.after != null && criteria.before != null ->
-                itemRepository.findByTimeBetween(criteria.after, criteria.before)
-
-            criteria.after != null ->
-                itemRepository.findByTimeAfter(criteria.after)
-
-            criteria.before != null ->
-                itemRepository.findByTimeBefore(criteria.before)
-
-            criteria.name != null ->
-                itemRepository.findByNameContainsIgnoreCase(criteria.name)
-
-            else -> itemRepository.findAll()
+        criteria.name?.let {
+            query.addCriteria(Criteria.where("name").regex(it, "i"))
         }
+
+        val timeCriteria = mutableListOf<Criteria>()
+        criteria.after?.let { timeCriteria.add(Criteria.where("time").gte(it)) }
+        criteria.before?.let { timeCriteria.add(Criteria.where("time").lte(it)) }
+
+        if (timeCriteria.isNotEmpty()) {
+            query.addCriteria(Criteria().andOperator(*timeCriteria.toTypedArray()))
+        }
+
+        return mongoTemplate.find(query, Item::class.java)
     }
 }
